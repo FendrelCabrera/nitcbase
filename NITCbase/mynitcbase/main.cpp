03d0688232dafac767e5f1037ce3dbd874371152
FendrelCabrera <shalin36shayne@gmail.com>
2024-01-04 18:06:39 +0530

    Done

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -11,16 +11,12 @@ int main(int argc, char *argv[]) {
   // OpenRelTable cache;
 
   unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
-
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2 << std::endl;
+  for(int i = 0; i < 4; i++) {
+    Disk::readBlock(buffer, i);
+    for(int x: buffer)
+      std::cout << x;
+    std::cout << std::endl;
+  }
 
   // return FrontendInterface::handleFrontend(argc, argv);
   return 0;

0ea62daeacf12d998ed62f874ecadec4ef14ed26
FendrelCabrera <shalin36shayne@gmail.com>
2024-01-08 22:41:29 +0530

    half done

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,5 +1,4 @@
 #include "BlockBuffer.h"
-
 #include <cstdlib>
 #include <cstring>
 
@@ -10,32 +9,65 @@ BlockBuffer::BlockBuffer(int blockNum) {
 RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
 int BlockBuffer::getHeader(struct HeadInfo *head) {
-    unsigned char buffer[BLOCK_SIZE];
+    /*
+    Used to get the header of the block into the location pointed to by `head`
+    NOTE: this function expects the caller to allocate memory for `head`
+    */
+    unsigned char*bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
 
-    Disk::readBlock(buffer, this->blockNum);
+    if(ret != SUCCESS)
+        return ret;
 
-    memcpy(&head->numSlots, buffer + 24, 4);
-    memcpy(&head->numEntries, buffer + 16, 4);
-    memcpy(&head->numAttrs, buffer + 20, 4);
-    memcpy(&head->rblock, buffer + 12, 4);
-    memcpy(&head->lblock, buffer + 8, 4);
+    memcpy(&head->numSlots, bufferPtr + 24, 4);
+    memcpy(&head->numEntries, bufferPtr + 16, 4);
+    memcpy(&head->numAttrs, bufferPtr + 20, 4);
+    memcpy(&head->rblock, bufferPtr + 12, 4);
+    memcpy(&head->lblock, bufferPtr + 8, 4);
 
     return SUCCESS;
 }
 
 int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+    /*
+    Used to get the record at slot `slotNum` into the array `rec`
+    NOTE: this function expects the caller to allocate memory for `rec`
+    */
+
     struct HeadInfo head;
     this->getHeader(&head);
 
     int attrCount = head.numAttrs;
     int slotCount = head.numSlots;
 
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer, this->blockNum);
+    unsigned char *bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if(ret != SUCCESS)
+        return ret;
 
     int recordSize = attrCount*ATTR_SIZE;
-    unsigned char *slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize*slotNum);
+    unsigned char *slotPointer = bufferPtr + HEADER_SIZE + slotCount + (recordSize*slotNum);
 
     memcpy(rec, slotPointer, recordSize);
     return SUCCESS;
+}
+
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+  // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+  int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+  if (bufferNum == E_BLOCKNOTINBUFFER) {
+    bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+    if (bufferNum == E_OUTOFBOUND) {
+      return E_OUTOFBOUND;
+    }
+
+    Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+  }
+
+  // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+  *buffPtr = StaticBuffer::blocks[bufferNum];
+
+  return SUCCESS;
 }
